6653fd723fc5394d9e212af1e1ae8cfbeb2a1ba0
zikong <jlzikong@gmial.com>
2014-01-06 10:34:39 +0800

    Initial Commit

---
+++ javaScriptCore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  javaScriptCore
+//
+//  Created by 紫空 on 14-1-6.
+//  Copyright (c) 2014年 zikong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ javaScriptCoreTests/javaScriptCoreTests.m
@@ -0,0 +1,34 @@
+//
+//  javaScriptCoreTests.m
+//  javaScriptCoreTests
+//
+//  Created by 紫空 on 14-1-6.
+//  Copyright (c) 2014年 zikong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface javaScriptCoreTests : XCTestCase
+
+@end
+
+@implementation javaScriptCoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
